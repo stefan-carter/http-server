6489b85d5674558cc2ffd916fbf0a470e8642644
stefan-carter <stefan.carter@icloud.com>
2022-03-14 16:42:16 +0000

    first commit

---
+++ src/main/java/co/uk/barclays/httpserver/HttpServerApplication.java
@@ -0,0 +1,13 @@
+package co.uk.barclays.httpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/uk/barclays/httpserver/HttpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.barclays.httpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
